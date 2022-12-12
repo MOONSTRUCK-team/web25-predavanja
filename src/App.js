e0979082ab90cd85d88651e662f8575d6e6c9a05
Bogdan Ilic <ilic.bogdan95@gmail.com>
2022-12-12 19:52:48 +0100

    Povezivanje Metamask, Ethers, Contract i FE

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
+  Button,
+  useToast
 } from '@chakra-ui/react';
+
+import {
+  LinkIcon
+} from '@chakra-ui/icons';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Counter from './counter/Counter';
+import ProviderWrapper from './ProviderWrapper';
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const toast = useToast({ duration: 4000, isClosable: true });
+
+  const providerWrapper = new ProviderWrapper(onAccountsChanged);
+
+  useEffect(() => {
+    onAccountsChanged();
+  }, []);
+
+  function connectToMetamask() {
+    providerWrapper.connectToMetamask(() => {
+      toast({ title: 'We are connected'});
+      setConnected(true);
+    }, () => {
+      toast({ title: 'Something went wrong!'});
+      setConnected(false);
+    })
+  }
+
+  function onAccountsChanged() {
+    providerWrapper.isConnected().then((connected) => setConnected(connected));
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+              <Button 
+              leftIcon={<LinkIcon/>}
+              p={4}
+              color='teal'
+              size='lg'
+              disabled={connected}
+              onClick={connectToMetamask}>Connect</Button>
+              <Counter isConnected={connected} signer={providerWrapper.getSigner()} />
           </VStack>
         </Grid>
       </Box>
