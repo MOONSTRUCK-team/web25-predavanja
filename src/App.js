a40971fccbfaed197959c5e048f323a89a91362e
Bogdan Ilic <ilic.bogdan95@gmail.com>
2022-12-15 18:45:24 +0100

    Testing out IPFS JSON upload

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
 import Header from './Header';
 import NFTGrid from './NFTGrid';
 
+import uploadJSON  from './IPFS';
+
 function App() {
+  uploadJSON({"name": "Test2"});
   return (
     <ChakraProvider theme={theme}>
       <Box>
